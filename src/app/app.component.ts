e00f3880364bb6ab6fc67d3329b824d01fd4c95e
C <70753331+Deusanod@users.noreply.github.com>
2021-01-05 20:04:09 +0100

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,15 @@ export class AppComponent {
   constructor() {
     // Your web app's Firebase configuration
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    //TODO Q1
     const firebaseConfig = {
-      // TODO Q1
+      apiKey: "AIzaSyC-SoQ1WQcVSUGFqB4r7Q9L--c_ouWKRgo",
+      authDomain: "something-fa053.firebaseapp.com",
+      projectId: "something-fa053",
+      storageBucket: "something-fa053.appspot.com",
+      messagingSenderId: "197017811078",
+      appId: "1:197017811078:web:2a6b13eb5cdda17b6fb907",
+      databaseURL: "https://something-fa053-default-rtdb.europe-west1.firebasedatabase.app/"
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
